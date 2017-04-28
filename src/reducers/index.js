758a7ba250c21e5f1a550dc6345cf1981a946e0c
wdh <wdh@marketlogicsoftware.com>
2017-04-28 16:10:21 +0200

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import track from './track';
+import auth from './auth';
+
+export default combineReducers({
+  auth,
+  track,
+  routing: routerReducer
+});
